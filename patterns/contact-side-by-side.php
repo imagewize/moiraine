edbf4793d352b4cb8068cc6f1f8261f742caccf2
Jasper Frumau <jasper@imagewize.com>
2025-03-18 13:21:01 +0700

    Contact Side by Side Stub

---
+++ patterns/contact-side-by-side.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Title: Contact Side by Side
+ * Slug: moiraine/contact-side-by-side
+ * Description: Contact information displayed in a side-by-side layout with grid blocks
+ * Categories: moiraine/card
+ * Keywords: card, contact, social, links, email, company, side-by-side
+ * Viewport Width: 1200
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Contact Card Side by Side"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|large","bottom":"var:preset|spacing|large","left":"var:preset|spacing|large"},"blockGap":"var:preset|spacing|medium"},"border":{"radius":"5px"}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)">
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":"var:preset|spacing|large"}}} -->
+<div class="wp-block-columns">
+
+<!-- wp:column {"width":"40%","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);flex-basis:40%">
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:600">Get in touch</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.</p>
+<!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"60%"} -->
+<div class="wp-block-column" style="flex-basis:60%">
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-columns">
+
+<!-- wp:column -->
+<div class="wp-block-column">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}},"border":{"radius":"5px"}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)">
+<!-- wp:heading {"level":4,"style":{"typography":{"fontWeight":"600"}}} -->
+<h4 class="wp-block-heading" style="font-weight:600">Collaborate</h4>
+<!-- /wp:heading -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">partner@example.com</p>
+<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}},"border":{"radius":"5px"}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)">
+<!-- wp:heading {"level":4,"style":{"typography":{"fontWeight":"600"}}} -->
+<h4 class="wp-block-heading" style="font-weight:600">Press</h4>
+<!-- /wp:heading -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">press@example.com</p>
+<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:column -->
+
+</div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--medium)">
+
+<!-- wp:column -->
+<div class="wp-block-column">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}},"border":{"radius":"5px"}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)">
+<!-- wp:heading {"level":4,"style":{"typography":{"fontWeight":"600"}}} -->
+<h4 class="wp-block-heading" style="font-weight:600">Join Our Team</h4>
+<!-- /wp:heading -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">careers@example.com</p>
+<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}},"border":{"radius":"5px"}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)">
+<!-- wp:heading {"level":4,"style":{"typography":{"fontWeight":"600"}}} -->
+<h4 class="wp-block-heading" style="font-weight:600">Say Hello</h4>
+<!-- /wp:heading -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">hello@example.com</p>
+<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:column -->
+
+</div>
+<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
+
+</div>
+<!-- /wp:column -->
+
+</div>
+<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
