bc96d28586ac818ed06a07daecc344f39c675f42
Jasper Frumau <jasper@imagewize.com>
2025-03-18 12:15:24 +0700

    Contact & About Page updates

---
+++ patterns/page-contact.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Contact Page
+ * Slug: moiraine/page-contact
+ * Description: A full page design for a Contact page with contact details and form
+ * Categories: moiraine/pages
+ * Keywords: page, layout, template,contact, form, email, phone, address
+ * Viewport Width: 1500
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:pattern {"slug":"moiraine/contact-details"} /-->
+
+<!-- wp:pattern {"slug":"moiraine/team-members"} /-->
+
+<!-- wp:pattern {"slug":"moiraine/numbers-stacked"} /-->
