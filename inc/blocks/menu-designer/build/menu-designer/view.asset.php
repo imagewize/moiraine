3195fefc7f75a3169d6f72dd897c05b586edf281
Jasper Frumau <jasper@imagewize.com>
2025-09-22 14:11:40 +0700

    build view asset file.

--- inc/blocks/menu-designer/build/menu-designer/view.asset.php
+++ inc/blocks/menu-designer/build/menu-designer/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-interactivity'), 'version' => 'ba4a4ae742aac55cb5e4');
+<?php return array('dependencies' => array('wp-interactivity'), 'version' => '9a5f1a2b5f1345b019c4');
