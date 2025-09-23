a75851714db241edceb45d7e047c103853bfbea6
Jasper Frumau <jasper@imagewize.com>
2025-09-23 09:41:51 +0700

    modern CSS container queries approach

--- inc/blocks/menu-designer/build/menu-designer/view.asset.php
+++ inc/blocks/menu-designer/build/menu-designer/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'e1e79d53390fa1c8db6a', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'ed112fd5466b30db4754', 'type' => 'module');
